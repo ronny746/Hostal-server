0c22b816b7692ebd95d44a0864ad5098c7e4dee4
ronny746 <rkrana6631@gmail.com>
2024-12-20 19:47:01 +0530

    update db

--- .history/config/database_20241207185801.js
+++ .history/config/database_20241207185801.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb+srv://hostal:CazvKvGIwXd2o8G9@cluster0.lorer.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', { useNewUrlParser: true, useUnifiedTopology: true });
+        await mongoose.connect('mongodb://127.0.0.1:27017/hostal', { useNewUrlParser: true, useUnifiedTopology: true });
         console.log('MongoDB connected');
     } catch (error) {
         console.error('Database connection failed:', error.message);
--- .history/config/database_20241220193924.js
+++ .history/config/database_20241220193924.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb+srv://hostal:CazvKvGIwXd2o8G9@cluster0.lorer.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', { useNewUrlParser: true, useUnifiedTopology: true });
+        await mongoose.connect('mongodb+srv://maanhimagi:maanhimagi@cluster0.lorer.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', { useNewUrlParser: true, useUnifiedTopology: true });
         console.log('MongoDB connected');
     } catch (error) {
         console.error('Database connection failed:', error.message);
