9f880dd282053fc3b891b6c44093082d042fa598
Garvita <garvitasingh>
2025-01-20 12:18:33 +0530

    room booking

--- models/room.model.js
+++ models/room.model.js
@@ -1,29 +1,27 @@
 const mongoose = require('mongoose');
 
 const RoomSchema = new mongoose.Schema({
-    roomType: {
-        type: String,
-        required: true,
-        enum: [
-          'Single Bedded', 
-          'Double Bedded',
-          'Triple Bedded',
-          'Four Bedded',
-          'AC Single Bedded',
-          'AC Double Bedded',
-          'AC Triple Bedded',
-          'AC Four Bedded',
-          'Classic',
-          'Deluxe',
-          'Super Deluxe',
-        ], 
-      },
-      totalRooms: { type: Number, required: true }, 
-      guestAllowedPerRoom: { type: Number, required: true },
-      description: { type: String }, 
-      images: [{ type: String }], 
-      ratePerDay: { type: Number, required: true },  
-    hostal: { type: mongoose.Schema.Types.ObjectId, ref: 'Hostel', required: true },
+  roomType: {
+    type: String,
+    required: true,
+    enum: [
+      'Single Bedded',
+      'Double Bedded',
+      'Triple Bedded',
+      'Four Bedded',
+      'Classic',
+      'Deluxe',
+      'Super Deluxe',
+    ],
+  },
+  totalRooms: { type: Number, required: true },
+  guestAllowedPerRoom: { type: Number, required: true },
+  description: { type: String },
+  images: [{ type: String }],
+  ratePerDay: { type: Number, required: true },
+  hostal: { type: mongoose.Schema.Types.ObjectId, ref: 'Hostel', required: true },
+  bookedRooms: { type: Number, default: 0 },
+  availableRooms: { type: Number, default: 0 },
 }, { timestamps: true });
 
 module.exports = mongoose.model('Room', RoomSchema);
