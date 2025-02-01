37966a1632a88a25325d1ee2aba2e68caa311cc2
ronny746 <rkrana6631@gmail.com>
2025-02-01 22:48:00 +0530

    update

---
+++ .history/controllers/paymentController_20250201224722.js
@@ -0,0 +1,56 @@
+import dotenv from "dotenv";
+dotenv.config();  // Load environment variables from .env
+
+import axios from 'axios';
+
+// Function to generate a random order ID
+function generateOrderId() {
+    const prefix = 'Hostelio';
+    const randomNumber = Math.random().toString(36).substring(2, 2 + 12); // Generates a random number between 100 and 999
+    return prefix + randomNumber;
+}
+
+export const createOrder = async (req, res) => {
+    try {
+        // Generate a random order ID
+        const orderId = generateOrderId();
+
+        // Add the generated order ID to the request body
+        req.body.order_id = orderId;
+
+        // Convert the modified request body to a JSON string
+        const data = JSON.stringify(req.body);
+
+        // Configuration for the Axios request
+        const config = {
+            method: 'post',
+            maxBodyLength: Infinity,
+            url: 'https://sandbox.cashfree.com/pg/orders',
+            headers: {
+                'x-client-id': "TEST1045882831b47a921e4f3131c4f582885401",
+                'x-client-secret': "cfsk_ma_test_0f9601ad3835b3f97f8665c7ebc7905c_af366842",
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'x-api-version': '2023-08-01'
+            },
+            data: data
+        };
+
+        // Make the request to the Cashfree API
+        const response = await axios.request(config);
+
+        // Log the response data
+
+        // Return the response data and status code to the client
+        res.status(response.status).json(response.data);
+    } catch (error) {
+        // Handle errors
+        if (error.response) {
+            console.error('Error Response:', error.response.data);
+            res.status(error.response.status).json(error.response.data);
+        } else {
+            console.error('Error:', error.message);
+            res.status(500).json({ message: 'Internal Server Error', error: error.message });
+        }
+    }
+};
