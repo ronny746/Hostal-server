8e5abe27dc8582db2cf31f406b1278235cd9f7de
ronny746 <rkrana6631@gmail.com>
2025-02-24 19:36:46 +0530

    update

--- .history/config/database_20250224193603.js
+++ .history/config/database_20250224193603.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb+srv://hostal:CazvKvGIwXd2o8G9@cluster0.lorer.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+        await mongoose.connect(process.env.MONGO_URI, {
             serverSelectionTimeoutMS: 30000, // Increase timeout to 30s
             socketTimeoutMS: 45000, // Increase socket timeout
         });
